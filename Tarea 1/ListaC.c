196c4c350ae253ce8f00bcb6e4415e4f17a5ffe6
Oscar <43174140+olgt@users.noreply.github.com>
2020-08-09 10:37:00 -0600

    Add files via upload

---
+++ Tarea 1/ListaC.c
@@ -0,0 +1,140 @@
+#include "ListaC.h"
+
+    void add(ListaC* l,NodoC* nuevo){
+    if(l->head==NULL){
+        l->head=nuevo;
+    }else if(nuevo->val<=l->head->val){
+        nuevo->derecha=l->head;
+        l->head->izquierda=nuevo;
+        l->head=nuevo;
+    }else{
+        NodoC* aux=l->head;
+        while(aux->derecha!=NULL){
+            if(aux->val>nuevo->val){
+                    NodoC* ant=aux->izquierda;
+                    ant->derecha=nuevo;
+                    nuevo->izquierda=ant;
+                    nuevo->derecha=aux;
+                    aux->izquierda=nuevo;
+                    return;
+                }else if(nuevo->val<=aux->derecha->val){
+                    NodoC *siguiente=aux->derecha;
+                    aux->derecha=nuevo;
+                    nuevo->derecha=aux;
+                    nuevo->derecha=siguiente;
+                    siguiente->izquierda=nuevo;
+                    return;
+                }
+            aux=aux->derecha;
+        }
+        aux->derecha=nuevo;
+        nuevo->izquierda=aux;
+    }
+}
+
+void Liberar(ListaC* l){
+    NodoA* aux=l->head;
+    NodoA* tmp;
+    while(aux!=NULL){
+        tmp=aux->derecha;
+        free(aux);
+        aux=tmp;
+    }
+}
+
+void imprimir(ListaC* l){
+    NodoA* aux=l->head;
+    while(aux!=NULL){
+        printf("El valor es %i \n",aux->val);
+        aux=aux->derecha;
+    }
+    printf("\n");
+}
+ListaC* nuevaLista(){
+    ListaC* nueva=(ListaC*) malloc(sizeof(ListaC));
+    nueva->head=NULL;
+    return nueva;
+}
+void generar(ListaC* l){
+    FILE * fp;
+    fp=fopen("./cadigo.txt","w");
+    if(fp==NULL){
+        printf("Error");
+        return;
+    }
+    char cad[1024];
+    sprintf(cad,"digraph G{ \n node[shape=\"box\"];\n");
+    fputs(cad,fp);
+    NodoC* aux;
+    aux=l->head;
+    guardarRecursivo(aux,fp,cad);
+    sprintf(cad,"}\n");
+    fputs(cad,fp);
+    fclose(fp);
+}
+
+void guardarRecursivo(NodoC* aux,FILE* fp,char c[]){
+    if(aux==NULL){
+        return;
+    }else{
+        if(aux->derecha!=NULL){
+
+            sprintf(c,"node%p[label=\"%i\"]\n",&(*aux),aux->val);
+            fputs(c,fp);
+            guardarRecursivo(aux->derecha,fp,c);
+            sprintf(c,"node%p->node%p;\n node%p->node%p;\n",&(*aux),&(*aux->derecha),&(*aux->derecha),&(*aux));
+            fputs(c,fp);
+        }else{
+            sprintf(c,"node%p[label=\"%i\"]\n",&(*aux),aux->val);
+            fputs(c,fp);
+        }
+    }
+}
+
+void borrarNodo(ListaC* l, int posicionLista){
+    NodoC* aux = l->head;
+    int posicionActual = 1;
+
+    while(aux != NULL){
+        if(posicionActual == posicionLista && aux != NULL){
+            if(aux == l->head){
+                l->head = aux->derecha;
+                aux->derecha = NULL;
+                aux = NULL;
+                printf("Nodo Encontrado Borrado: ");
+                printf("%d \n", posicionActual);
+                break;
+            }
+            else if(aux->derecha == NULL){
+                aux->izquierda->derecha = NULL;
+                aux=NULL;
+                printf("Nodo Encontrado Borrado: ");
+                printf("%d \n", posicionActual);
+                break;
+            }
+            else {
+                aux->izquierda->derecha = aux->derecha;
+                aux->derecha->izquierda = aux->izquierda;
+                aux = NULL;
+                printf("Nodo Encontrado Borrado: ");
+                printf("%d \n", posicionActual);
+                break;
+            }
+        }
+        posicionActual++;
+        aux = aux->derecha;
+    }
+}
+
+void getNodo(ListaC* l, int valorNodo){
+    NodoC* aux = l->head;
+    int valorActual = aux->val;
+
+    while(aux != NULL){
+        if(aux->val == valorNodo){
+            printf("Valor Encontrado: %d \n", valorNodo);
+            break;
+        }
+        aux = aux->derecha;
+    }
+}
---
+++ Tarea 1/main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "ListaC.h"
+#include "NodoC.h"
+
+int main(){
+    NodoA* a=newNodoC(1);
+    NodoA* b=newNodoC(2);
+    NodoC* c=newNodoC(3);
+    NodoC* d=newNodoC(4);
+    NodoC* e=newNodoC(5);
+    NodoC* f=newNodoC(6);
+    NodoC* g=newNodoC(7);
+    NodoC* h=newNodoC(8);
+    NodoC* i=newNodoC(9);
+    NodoC* j=newNodoC(10);
+
+    ListaC* l=nuevaLista();
+
+    add(l,a);
+    add(l,b);
+    add(l,c);
+    add(l,d);
+    add(l,e);
+    add(l,f);
+    add(l,g);
+    add(l,h);
+    add(l,i);
+    add(l,j);
+
+    borrarNodo(l, 3);
+    getNodo(l, 4);
+
+    imprimir(l);
+    generar(l);
+    Liberar(l);
+    free(l);
+    return 0;
+}
